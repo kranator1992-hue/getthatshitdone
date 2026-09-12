4753e40e4099d10e16c13285dc4737f64960ca6f
kranator1992-hue <kranator1992@gmail.com>
2026-09-12 16:14:57 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,47 @@
+// Service Worker des UB-Planers. Geltungsbereich: nur dieser Unterordner.
+const CACHE = "ubplaner-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./apple-touch-icon.png"
+];
 
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", e => {
+  const req = e.request;
+  if(req.method !== "GET") return;
+
+  if(req.mode === "navigate"){
+    e.respondWith(caches.match("./index.html").then(r => r || fetch(req)));
+    return;
+  }
+
+  e.respondWith(
+    caches.match(req).then(hit => {
+      if(hit) return hit;
+      return fetch(req).then(res => {
+        if(res && res.ok && res.type === "basic"){
+          const copy = res.clone();
+          caches.open(CACHE).then(c => c.put(req, copy));
+        }
+        return res;
+      }).catch(() => hit);
+    })
+  );
+});
